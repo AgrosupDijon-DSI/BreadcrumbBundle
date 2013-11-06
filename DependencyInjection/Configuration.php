4ee67fdd5ca98e8a667064958f25153d0254ce27
Valérian Girard <valerian.girard@educagri.fr>
2013-11-06 09:10:37 +0100

    Remove unused variable

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cnerta_breadcrumb')
+        $treeBuilder->root('cnerta_breadcrumb')
                 ->children()
                     ->arrayNode('twig')
                         ->addDefaultsIfNotSet()
